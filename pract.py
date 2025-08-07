0eb6055e9381d30440c320aa12c9a88119b0dfce
Atif Khan <wahabshah10200@gmail.com>
2025-08-07 16:50:37 +0500

    day2 of learning numpy

--- pract.py
+++ pract.py
@@ -32,3 +32,55 @@ print("\n")
 # we can also use full_like to fully initialize an array with shape like anyother array
 print(np.full_like(b,-20))
 
+print(np.random.rand(4,5))
+
+print(np.random.randint(4,10,size = (3,3)))
+
+# Repeat 
+
+arr = np.array([[1,2,3]])
+r1 = np.repeat(arr,3,axis = 0)
+print(r1)
+
+
+
+# exersize
+
+new_arr = np.ones((5,5))
+new_arr[1:-1,1:-1] = 0
+new_arr[2,2] = 9
+print(new_arr)
+
+# copying array
+
+a = np.array([1,2,3])
+b = a.copy()
+b[0] = 100
+print(a)
+
+
+# mathematics
+
+# simple arithimatics
+a = np.array([1,2,3,4])
+# a /= 2
+print(a)
+
+# trig functions
+print(np.sin(a))
+print(np.cos(a))
+
+# linear algerbra 
+mat1 = np.ones((2,3))
+mat2 = np.full((3,2),2)
+
+print(np.matmul(mat1,mat2))
+
+# find determinant
+mat3 = np.identity(3)
+print(mat3)
+print(np.linalg.det(mat3))
+
+
+
+
